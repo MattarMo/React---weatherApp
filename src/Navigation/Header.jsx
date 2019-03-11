803a4964fd757229048d0430fc66b5ea154eaa9b
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-03-11 18:38:45 -0400

    adding header(nav) and footer

---
+++ src/Navigation/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; //use for Link tag instead of a tag
+
+const HeaderNavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/" className="navbar-brand">
+        Weather
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Current Weather
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login-form" className="nav-link">
+              Login form
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderNavBar;
