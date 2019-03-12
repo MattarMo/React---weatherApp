98a85a84bd309b6cae07dc07df679cfd5bd592c0
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-03-12 08:34:31 -0400

    code refactoring, ridding of Core component used for routing

--- src/Containers/Root.jsx
+++ src/Containers/Root.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Core from './Core.jsx';
+import Header from '../Navigation/Header.jsx';
+import CurrentWeather from '../components/CurrentWeather.jsx';
+import Footer from '../Navigation/Footer.jsx';
 
 export default class Root extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Core} />
-        </Switch>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={CurrentWeather} />
+          </Switch>
+          <Footer />
+        </div>
       </BrowserRouter>
     );
   }
