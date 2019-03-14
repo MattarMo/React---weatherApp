f9d29657f2bc2e260001efe59aecfc4f1362977a
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-03-14 12:52:10 -0400

    new file names

---
+++ src/components/CurrentWeather/Weather.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import EnterLocationWeather from './EnterLocationWeather.jsx';
+import DisplayWeather from './DisplayWeather.jsx';
+
+const API_KEY = '3958ce557d2539d832f7953ea777aa82';
+
+export default class CurrentWeather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Get your Current Weather for today',
+      datetime: undefined,
+      temperature: undefined,
+      description: undefined,
+      icon: undefined,
+      error: undefined
+    };
+
+    this.currentGetWeatherCall = this.currentGetWeatherCall.bind(this);
+  }
+
+  currentGetWeatherCall(e) {
+    e.preventDefault();
+    const getCity = e.target.city.value;
+    const getCountry = e.target.country.value;
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${getCity},${getCountry}&appid=${API_KEY}&units=imperial`
+      )
+      .then(res => {
+        const weatherData = res.data;
+        console.log(weatherData);
+        this.setState({
+          title: 'Get your Current Weather for today',
+          temperature: weatherData.main.temp,
+          description: weatherData.weather[0].description,
+          icon: weatherData.weather[0].icon,
+          error: ''
+        });
+      })
+      .catch(() => {
+        this.setState({
+          title: 'Get your Current Weather for today',
+          temperature: undefined,
+          description: undefined,
+          icon: undefined,
+          error:
+            'Please enter a city and country(must be a real and matching city to country)'
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <EnterLocationWeather
+          currentGetWeatherCall={this.currentGetWeatherCall}
+          title={this.state.title}
+        />
+        <DisplayWeather
+          temperature={this.state.temperature}
+          description={this.state.description}
+          icon={this.state.icon}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/ForeCastWeather/DisplayForecast.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ForecastDisplay = props => {
+  console.log(props.forecastData);
+
+  return (
+    <div className="container">
+      {props.error && <p className="text-danger">{props.error}</p>}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Date/Time</th>
+            <th scope="col">Temperature</th>
+            <th scope="col">WeatherIcon</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.forecastData.map(forecast => {
+            return (
+              <tr className="table" key={forecast.dt}>
+                <td>{forecast.dt_txt}</td>
+                <td>{forecast.main.temp}&deg;F</td>
+                <td>
+                  <img
+                    src={`http://openweathermap.org/img/w/${
+                      forecast.weather[0].icon
+                    }.png`}
+                    alt={forecast.dt_txt}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+ForecastDisplay.propTypes = {
+  forecastData: PropTypes.array.isRequired,
+  error: PropTypes.string
+};
+
+export default ForecastDisplay;
