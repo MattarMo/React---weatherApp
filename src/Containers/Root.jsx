51f271e417b1dfa1bed43996473799ec6b31de02
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-04-30 16:31:10 -0400

    added store/provider to root of app

--- src/Containers/Root.jsx
+++ src/Containers/Root.jsx
@@ -4,23 +4,27 @@ import Header from '../Navigation/Header.jsx';
 import Forecast from '../Components/ForeCastWeather/Forecast.jsx';
 import Weather from '../Components/CurrentWeather/Weather.jsx';
 import ErrorPath from '../components/ErrorPath.jsx';
+import { configureStore } from '../store/configureStore';
+import { Provider } from 'react-redux';
 
 export default class Root extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <br />
-          <br />
-          <br />
-          <Switch>
-            <Route exact path="/" component={Weather} />
-            <Route path="/forecast" component={Forecast} />
-            <Route component={ErrorPath} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider store={configureStore}>
+        <BrowserRouter>
+          <>
+            <Header />
+            <br />
+            <br />
+            <br />
+            <Switch>
+              <Route exact path="/" component={Weather} />
+              <Route path="/forecast" component={Forecast} />
+              <Route component={ErrorPath} />
+            </Switch>
+          </>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
